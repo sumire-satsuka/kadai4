50833559ecce5e199281adc06a0087cd3c193448
HOGE <hoge@example.com>
2023-08-01 14:41:13 +0000

    負の数のテストの条件文を適切にした

--- test_scripts/test.sh
+++ test_scripts/test.sh
@@ -49,23 +49,23 @@ else
   exit 1
 fi
 
-# 数値が自然数
+# 数値が小数点を含む
 result=$(./gcd.sh 3.4 4)
-if [[ $result = "Error: 引数は二つです。" ]] ; then
+if [[ $result = "Error: 小数点とか入力してはダメです。だって自然数だから。" ]] ; then
   echo "OK"
 else
   echo "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。"
   exit 1
 fi
 result=$(./gcd.sh 3.4 4.2)
-if [[ $result = "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。" ]] ; then
+if [[ $result = "Error: 小数点とか入力してはダメです。だって自然数だから。" ]] ; then
   echo "OK"
 else
   echo "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。"
   exit 1
 fi
 result=$(./gcd.sh 3 4.2)
-if [[ $result = "Error: 引数は二つです。" ]] ; then
+if [[ $result = "Error: 小数点とか入力してはダメです。だって自然数だから。" ]] ; then
   echo "OK"
 else
   echo  "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。"
@@ -73,21 +73,21 @@ else
 fi
 
 # 負の数
-result=$(./gcd.sh 3.4 4)
+result=$(./gcd.sh -3 4)
 if [[ $result = "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。" ]] ; then
   echo "OK"
 else
   echo "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。"
   exit 1
 fi
-result=$(./gcd.sh 3.4 4.2)
+result=$(./gcd.sh 3 -4)
 if [[ $result = "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。" ]] ; then
   echo "OK"
 else
   echo  "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。"
   exit 1
 fi
-result=$(./gcd.sh 3 4.2)
+result=$(./gcd.sh -3 -4)
 if [[ $result = "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。" ]] ; then
   echo "OK"
 else
