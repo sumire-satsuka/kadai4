ef57837d9ce56a8d3e96a5521b8d7f7d90f20ce4
HOGE <hoge@example.com>
2023-08-01 14:45:44 +0000

    testにNo.をつけた

--- test_scripts/test.sh
+++ test_scripts/test.sh
@@ -13,83 +13,83 @@
 # echo "hoge"
 
 
-# 2と4を入力する && 2を出力したら成功 || 出力しなかったらエラー
+# 1. 2と4を入力する && 2を出力したら成功 || 出力しなかったらエラー
 result=$(./gcd.sh 2 4)
 if [ $result = 2 ] ; then
-  echo "OK"
+  echo "1 OK"
 else
   echo "NG"
   exit 1
 fi
 
-# 3を入力する || エラーメッセージが出なかったらエラー
+# 2. 3を入力する || エラーメッセージが出なかったらエラー
 result=$(./gcd.sh 3)
 if [[ $result = "Error: 引数は二つです。" ]] ; then
-  echo "OK"
+  echo "2 OK"
 else
   echo "NG"
   exit 1
 fi
 
-# 文字を入力する || エラーメッセージが出なかったらエラー
+# 3. 文字を入力する || エラーメッセージが出なかったらエラー
 result=$(./gcd.sh aaa bbb)
 if [[ $result = "Error: 10進数の自然数を入れてね！" ]] ; then
-  echo "OK"
+  echo "3 OK"
 else
   echo "NG"
   exit 1
 fi
 
-# 引数の数が多い
+# 4. 引数の数が多い
 result=$(./gcd.sh 3 4 5 5)
 if [[ $result = "Error: 引数は二つです。" ]] ; then
-  echo "OK"
+  echo "4 OK"
 else
   echo "引数多すぎ"
   exit 1
 fi
 
-# 数値が小数点を含む
+# 5. 数値が小数点を含む
 result=$(./gcd.sh 3.4 4)
 if [[ $result = "Error: 小数点とか入力してはダメです。だって自然数だから。" ]] ; then
-  echo "OK"
+  echo "5-1 OK"
 else
   echo "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。"
   exit 1
 fi
 result=$(./gcd.sh 3.4 4.2)
 if [[ $result = "Error: 小数点とか入力してはダメです。だって自然数だから。" ]] ; then
-  echo "OK"
+  echo "5-2 OK"
 else
   echo "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。"
   exit 1
 fi
 result=$(./gcd.sh 3 4.2)
 if [[ $result = "Error: 小数点とか入力してはダメです。だって自然数だから。" ]] ; then
-  echo "OK"
+  echo "5-3 OK"
 else
   echo  "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。"
   exit 1
 fi
 
-# 負の数
+# 6. 負の数
 result=$(./gcd.sh -3 4)
 if [[ $result = "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。" ]] ; then
-  echo "OK"
+  echo "6-1 OK"
 else
   echo "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。"
   exit 1
 fi
 result=$(./gcd.sh 3 -4)
 if [[ $result = "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。" ]] ; then
-  echo "OK"
+  echo "6-2 OK"
 else
   echo  "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。"
   exit 1
 fi
 result=$(./gcd.sh -3 -4)
 if [[ $result = "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。" ]] ; then
-  echo "OK"
+  echo "6-3 OK"
 else
   echo  "Error: 最大公約数を求めるのに、なんで負の数入れたの？あなたが入れるべきは正の整数です。"
   exit 1
